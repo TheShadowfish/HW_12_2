70decc77f640f5946885bccba231c177a895d67f
Эдуард Кусков <shadowfish@yandex.ru>
2024-01-24 20:47:32 +0300

    tests fixed

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,22 @@
 from utils import arrs
+import pytest
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+@pytest.mark.parametrize('array, index, expected', [
+    ([1, 2, 3], 1, 2),
+    ([], 0, "test"),
+    ([1, 2, 3, 4], -1, 'test')
+])
+def test_get(array, index, expected):
+    assert arrs.get(array, index, "test") == expected
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+@pytest.mark.parametrize('array, start, end, expected', [
+    ([1, 2, 3], 1, 3, [2, 3]),
+    ([], 0, 100, []),
+    ([1, 2, 3, 4], -2, None, [3, 4])
+])
+def test_slice(array, start, end, expected):
+    assert arrs.my_slice(array, start, end) == expected
+
+
